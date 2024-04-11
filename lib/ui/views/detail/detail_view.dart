f7e2f201fbe55d3fd6437766eb0ddb3ef6bc6fe6
VincentVenan33 <92298629+VincentVenan33@users.noreply.github.com>
2024-04-12 01:07:29 +0700

    detail fix

--- lib/ui/views/detail/detail_view.dart
+++ lib/ui/views/detail/detail_view.dart
@@ -1,7 +1,6 @@
 import 'package:appsmobile/core/networks/detail/detail_product_network.dart';
 import 'package:appsmobile/core/view_model/detail/detail_product_page.dart';
 import 'package:appsmobile/core/view_model/view_model.dart';
-import 'package:appsmobile/ui/shared/loading.dart';
 import 'package:appsmobile/ui/shared/unfocus_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -38,30 +37,36 @@ class _DetailProductState extends ConsumerState<DetailProduct> {
         ),
         onModelReady: (DetailPageModel model) => model.initModel(),
         builder: (_, DetailPageModel model, __) {
-          return LoadingOverlay(
-            isLoading: model.busy,
-            child: UnfocusHelper(
-              child: Scaffold(
-                body: SingleChildScrollView(
-                  child: Container(
-                    child: Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(Icons.arrow_back),
+          return UnfocusHelper(
+            child: Scaffold(
+              backgroundColor: Colors.white,
+              body: Expanded(
+                child: model.busy
+                    ? const Expanded(
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      )
+                    : SingleChildScrollView(
+                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(24),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  icon: const Icon(Icons.arrow_back),
+                                ),
+                                Text(model.daftarproduct!.title),
+                              ],
+                            ),
                           ),
-                          Text(model.daftarproduct[widget.param.id!].title),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                ),
               ),
             ),
           );
