e493fcf174022fe2a6c45b2d84a34bf52fe36482
VincentVenan33 <92298629+VincentVenan33@users.noreply.github.com>
2024-04-14 15:24:28 +0700

    tes

--- lib/ui/views/detail/detail_view.dart
+++ lib/ui/views/detail/detail_view.dart
@@ -4,7 +4,6 @@ import 'package:appsmobile/core/networks/detail/detail_product_network.dart';
 import 'package:appsmobile/core/view_model/detail/detail_product_page.dart';
 import 'package:appsmobile/core/view_model/view_model.dart';
 import 'package:appsmobile/ui/shared/spacing.dart';
-import 'package:appsmobile/ui/shared/unfocus_helper.dart';
 import 'package:appsmobile/ui/views/cart_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -102,167 +101,164 @@ class _DetailProductState extends ConsumerState<DetailProduct> {
       ),
       onModelReady: (DetailPageModel model) => model.initModel(),
       builder: (_, DetailPageModel model, __) {
-        return UnfocusHelper(
-          child: Scaffold(
-            // backgroundColor: Colors.white,
-            appBar: AppBar(
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Cart(
-                          addCartProduct: addCartProduct,
-                          onQuantityChanged: (product, newQuantity) {
-                            setState(() {
-                              product.quantity = newQuantity;
-                            });
-                          },
-                        ),
+        return Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Cart(
+                        addCartProduct: addCartProduct,
+                        onQuantityChanged: (product, newQuantity) {
+                          setState(() {
+                            product.quantity = newQuantity;
+                          });
+                        },
                       ),
-                    );
-                  },
-                  icon: const Icon(Icons.shopping_cart),
-                ),
-              ],
-            ),
-            body: Container(
-              height: double.infinity,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('asset/image/NO8hx.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: RefreshIndicator(
-                onRefresh: () async {
-                  model.initModel();
+                    ),
+                  );
                 },
-                child: model.busy
-                    ? const Expanded(
-                        child: Center(
-                          child: CircularProgressIndicator(),
-                        ),
-                      )
-                    : SingleChildScrollView(
-                        child: Container(
-                          padding: const EdgeInsets.all(24),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                decoration: const BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(
-                                      10,
-                                    ),
+                icon: const Icon(Icons.shopping_cart),
+              ),
+            ],
+          ),
+          body: Container(
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('asset/image/NO8hx.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: RefreshIndicator(
+              onRefresh: () async {
+                model.initModel();
+              },
+              child: model.busy
+                  ? const Expanded(
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    )
+                  : SingleChildScrollView(
+                      child: Container(
+                        padding: const EdgeInsets.all(24),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    10,
                                   ),
                                 ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Image.network(
-                                      model.daftarproduct!.image,
-                                      height: 250,
-                                    ),
-                                  ],
-                                ),
                               ),
-                              Spacings.verSpace(15),
-                              Text(
-                                '\$ ${model.daftarproduct!.price}',
-                                style: const TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Spacings.verSpace(15),
-                              Text(
-                                model.daftarproduct!.title,
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Spacings.verSpace(15),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        Icons.star_rounded,
-                                        color: Colors.orange[600],
-                                      ),
-                                      Spacings.horSpace(5),
-                                      Text(
-                                        '${model.daftarproduct!.rating.rate} / ${model.daftarproduct!.rating.count}',
-                                        style: const TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.w400,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    model.daftarproduct!.category,
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w400,
-                                    ),
+                                  Image.network(
+                                    model.daftarproduct!.image,
+                                    height: 250,
                                   ),
                                 ],
                               ),
-                              Spacings.verSpace(15),
-                              const Text(
-                                'Description',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600,
-                                ),
+                            ),
+                            Spacings.verSpace(15),
+                            Text(
+                              '\$ ${model.daftarproduct!.price}',
+                              style: const TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
                               ),
-                              Text(model.daftarproduct!.description),
-                            ],
-                          ),
+                            ),
+                            Spacings.verSpace(15),
+                            Text(
+                              model.daftarproduct!.title,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Spacings.verSpace(15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star_rounded,
+                                      color: Colors.orange[600],
+                                    ),
+                                    Spacings.horSpace(5),
+                                    Text(
+                                      '${model.daftarproduct!.rating.rate} / ${model.daftarproduct!.rating.count}',
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  model.daftarproduct!.category,
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacings.verSpace(15),
+                            const Text(
+                              'Description',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(model.daftarproduct!.description),
+                          ],
                         ),
                       ),
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              // key: const Key("Add To Chat"),
-              shape: const CircleBorder(),
-              onPressed: () {
-                addToCart(
-                  AddProduct(
-                      id: model.daftarproduct!.id,
-                      title: model.daftarproduct!.title,
-                      image: model.daftarproduct!.image,
-                      price: model.daftarproduct!.price),
-                );
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Cart(
-                      addCartProduct: addCartProduct,
-                      onQuantityChanged: (product, newQuantity) {
-                        // Update the quantity of the product in the cart
-                        setState(() {
-                          product.quantity = newQuantity;
-                        });
-                      },
                     ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            // key: const Key("Add To Chat"),
+            shape: const CircleBorder(),
+            onPressed: () {
+              addToCart(
+                AddProduct(
+                    id: model.daftarproduct!.id,
+                    title: model.daftarproduct!.title,
+                    image: model.daftarproduct!.image,
+                    price: model.daftarproduct!.price),
+              );
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Cart(
+                    addCartProduct: addCartProduct,
+                    onQuantityChanged: (product, newQuantity) {
+                      // Update the quantity of the product in the cart
+                      setState(() {
+                        product.quantity = newQuantity;
+                      });
+                    },
                   ),
-                );
-              },
-              backgroundColor: Colors.cyan[200],
-              child: const Icon(
-                Icons.add_shopping_cart,
-                color: Colors.black,
-                size: 31.5,
-              ),
+                ),
+              );
+            },
+            backgroundColor: Colors.cyan[200],
+            child: const Icon(
+              Icons.add_shopping_cart,
+              color: Colors.black,
+              size: 31.5,
             ),
           ),
         );
